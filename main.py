6d8f6b56ed20b3f6798913ebf8ba183bd9833c16
Mrkorg1000 <korg1000@yandex.ru>
2023-02-22 13:55:06 +0300

    Switching to poetry commit

--- main.py
+++ main.py
@@ -32,4 +32,6 @@ main_api_router.include_router(
 app.include_router(main_api_router)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(
+        "main:app", host="localhost", port=8000, reload=True
+    )
