7ae026ff78122ee3719082e4d99c81eafaa8797a
Mrkorg1000 <80282966+Mrkorg1000@users.noreply.github.com>
2023-04-02 22:15:49 +0300

    price model commit (#19)
    * price model commit
    
    * Price model modified, default value added for 'validity_start'

--- database/models.py
+++ database/models.py
@@ -1,18 +1,39 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Numeric, Float, func, select
+from sqlalchemy import Column, Integer, String, ForeignKey, Numeric, Float, func, select, Date, and_
 from sqlalchemy.dialects.postgresql import UUID
 import uuid
 from sqlalchemy.dialects.mysql import FLOAT
 from sqlalchemy.orm import declarative_base, relationship, MapperProperty, column_property
+from datetime import date
 
 Base = declarative_base()
 
+
+class Price(Base):
+    __tablename__ = 'price'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    dish_price = Column(Float, nullable=False)
+    validity_start = Column(Date, nullable=False, default='1970-01-01')
+    validity_end = Column(Date, nullable=False)
+    dish_id = Column(
+        Integer,
+        ForeignKey("dish.id", ondelete="CASCADE")
+    )
+    dish = relationship(
+        'Dish', back_populates='price_values')
 
 class Dish(Base):
     __tablename__ = 'dish'
     id = Column(Integer, primary_key=True, autoincrement=True)
     title = Column(String, nullable=False)
     description = Column(String, nullable=False)
-    price = Column(Float)
+    price: MapperProperty =  column_property(
+        select(Price.dish_price)
+         .where(Price.dish_id == id)
+         .filter(and_(date.today() < Price.validity_end,
+                       date.today() > Price.validity_end))
+         .correlate_except(Price)
+         .scalar_subquery()
+    )
     submenu_id = Column(
         Integer,
         ForeignKey("submenu.id", ondelete="CASCADE")
@@ -20,6 +41,12 @@ class Dish(Base):
     submenu = relationship(
         'Submenu', back_populates='dishes')
     
+    price_values = relationship(
+        'Price', cascade="all, delete",
+        back_populates='dish'
+        )
+    
+
 
 class Submenu(Base):
     __tablename__ = 'submenu'
